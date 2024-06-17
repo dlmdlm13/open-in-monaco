e313ccdd31684f70248227b2467a5d802d338b5d
Matéo Demangeon <mdemangeon2@gmail.com>
2024-06-17 21:20:09 +0200

    import monaco

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import monaco from 'monaco-editor';
+
+monaco.editor.create(document.getElementById('container'), {
+    value: 'console.log("Hello, world!")',
+
+});
+
 console.log("OK");
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const MonacoEditorWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -13,10 +13,23 @@ module.exports = {
         new HtmlWebpackPlugin({
           template: './src/index.html',
         }),
-        // new CopyWebpackPlugin({
-        //   patterns: [
-        //     { from: 'src/manifest.json', to: '[name][ext]' },
-        //   ],
-        // }),
+        new MonacoEditorWebpackPlugin({
+          languages: ['javascript', 'typescript', 'json'],
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'src/manifest.json', to: 'manifest.json' }
+            ]
+          }),
     ],
+    devtool: false,
+    // devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
